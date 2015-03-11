d4fad603f127560f47fb95393d67c197951329c5
FcoManueel <FcoManueel@gmail.com>
2015-03-11 02:00:04 -0700

    added 'project' and 'task' scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 YoutubeNotes::Application.routes.draw do
+  resources :projects
+
+
+  resources :tasks
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
