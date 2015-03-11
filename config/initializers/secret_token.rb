19d78f1b8e76ebd7e82e3cd7c5222abfa3f97a4d
FcoManueel <FcoManueel@gmail.com>
2015-03-11 01:42:31 -0700

    First commit, rails skeleton Empty rails project created with no active record. Added mongoid, bson_ext and rails_12factor gems to Gemfile Added mongoid.yml with production (Heroku) configuration (first draft)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoutubeNotes::Application.config.secret_token = '0c61128ce8b16a89d7d09345f69cf05a18851e0cb165f9dca949941f72611a139fa4e1832a0010ea59d1d3d184852865e81c786e86986152b8d08d886564d18f'
